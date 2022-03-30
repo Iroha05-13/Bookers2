d6401e4a9458c378f850ec06bd3951cb30e0227c
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-03-30 16:02:15 +0000

    [Update-validation]Bookers2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, uniqueness: true, length: {minimum: 2, maximum: 20}
+  validates :introduction, length: {maximum: 50}
+
   has_one_attached :profile_image
 
   def get_profile_image(width, height)
