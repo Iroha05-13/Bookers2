e0e58a243f1e1dc5f53ca02a391b873dbe1f893f
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-04-02 12:25:06 +0000

    [Update Views]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @book = @user.books
+    @books = @user.books
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   def get_profile_image(width, height)
-    unless profile_image.attched?
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attch(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
