de7ce03385d8d8ce1e44c05a542bcaef6daaf69f
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-04-02 14:58:48 +0000

    [Update Views]Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,10 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
-    @user = @book.user.id
   end
 
   def edit
